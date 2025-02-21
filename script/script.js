6a4985b4f6e851cd214ac5c2a436cc2fe8512d51
GemsLister <jameslester.l.lopez@gmail.com>
2025-02-22 00:06:21 +0800

    feat: add buttons functions and create cardBody container/displays(title, author, pages, read) in a card

--- script/script.js
+++ script/script.js
@@ -9,6 +9,7 @@ const pagesInput = document.querySelector('#pages');
 const readInput = document.querySelector('#read');
 const notReadInput = document.querySelector('#not-read');
 // =================================================
+const cardContainer = document.querySelector('.card-container');
 
 
 // To show the form
@@ -34,17 +35,73 @@ function addBookToLibrary() {
   const title = titleInput.value;
   const author = authorInput.value;
   const pages = pagesInput.value;
-  
+
   let readOrNot = readInput.checked;
 
-  if(!readOrNot.checked){
-    readOrNot = notReadInput.checked;
+  if(!readOrNot){
+    readOrNot = false; //If it is not read
+  }
+  else if (readOrNot){
+    readOrNot = true; //If it is read
   }
 
   const newBook = new Book(title, author, pages, readOrNot);
   myLibrary.push(newBook);
+  myLibrary.forEach(book => console.log(book));
 
-  submitButton.addEventListener("click", () =>{
-    newBook.title();  
-  })
+
+  const cardBody = document.createElement('article');
+  cardBody.classList = 'card-body';
+
+  // Container for displays
+  const displayContainer = document.createElement('div');
+  displayContainer.classList = 'display-container';
+
+  // For displaying the title in container
+  const displayTitle = document.createElement('p');
+  displayTitle.textContent = `Title: ${title}`;
+
+  // For displaying the author in container
+  const displayAuthor = document.createElement('p');
+  displayAuthor.textContent = `Author: ${author}`;
+
+  // For displaying the pages in container
+  const displayPages = document.createElement('p');
+  displayPages.textContent = `Pages: ${pages}`;
+
+  // For displaying the read/not-read in container
+  const displayReadStatus = document.createElement('button');
+  if(!readOrNot){
+    displayReadStatus.textContent = 'Not Read';
+  }
+  else{
+    displayReadStatus.textContent = 'Read';
+  }
+
+  // displayReadStatus.addEventListener("click", () =>{
+  //   if()
+  // })
+
+  // insert displays in display-container
+  displayContainer.append(displayTitle, displayAuthor, displayPages, displayReadStatus);
+
+  // insert display-container in card-body
+  cardBody.appendChild(displayContainer);
+
+  // insert card-body in card-container
+  cardContainer.appendChild(cardBody);
+
+  // Clear the form when it is submitted
+  titleInput.value = '';
+  authorInput.value = '';
+  pagesInput.value = '';
+  readInput.checked = '';
+  notReadInput.checked = '';
+  form.style.display = 'none';
+  // ===================================
 }
+
+submitButton.addEventListener("click", (event) =>{
+  event.preventDefault(); //To prevent default form submission
+  addBookToLibrary();
+})
