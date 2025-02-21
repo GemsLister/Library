226b8c4442979c8d2ef32eaaf1963e3b50487024
GemsLister <jameslester.l.lopez@gmail.com>
2025-02-21 21:41:53 +0800

    feat: add 'Book' and 'addBookToLibrary' functions, and myLibrary array

--- script/script.js
+++ script/script.js
@@ -1,6 +1,15 @@
 const showFormButton = document.querySelector('#show-form-btn');
+const submitButton = document.querySelector('#submit-btn');
 const form = document.querySelector('#form');
 const closeButton = document.querySelector('#close-btn');
+// Inputs (Title, Author, Pages, Read/Not)
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const readInput = document.querySelector('#read');
+const notReadInput = document.querySelector('#not-read');
+// =================================================
+
 
 // To show the form
 showFormButton.addEventListener("click", () =>{
@@ -11,3 +20,31 @@ showFormButton.addEventListener("click", () =>{
 closeButton.addEventListener("click", () =>{
     form.style.display = 'none';
 })
+
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  const title = titleInput.value;
+  const author = authorInput.value;
+  const pages = pagesInput.value;
+  
+  let readOrNot = readInput.checked;
+
+  if(!readOrNot.checked){
+    readOrNot = notReadInput.checked;
+  }
+
+  const newBook = new Book(title, author, pages, readOrNot);
+  myLibrary.push(newBook);
+
+  submitButton.addEventListener("click", () =>{
+    newBook.title();  
+  })
+}
